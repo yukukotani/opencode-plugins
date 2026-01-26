d18b0db838a8e7fc65c6a1c28df7480a3552baa6
Yuku Kotani <yukukotani@gmail.com>
2026-01-26 15:03:06 +0900

    skip child

--- plugins/notification.ts
+++ plugins/notification.ts
@@ -1,4 +1,4 @@
-import type { Plugin } from "@opencode-ai/plugin";
+import type { Plugin, PluginInput } from "@opencode-ai/plugin";
 
 export const NotificationPlugin: Plugin = async ({
   project,
@@ -12,8 +12,26 @@ export const NotificationPlugin: Plugin = async ({
     event: async ({ event }) => {
       // Send notification on session completion
       if (event.type === "session.idle") {
+        const isChild = await isChildSession(
+          client,
+          event.properties.sessionID,
+        );
+        if (isChild) {
+          return;
+        }
+
         await $`afplay /System/Library/Sounds/Funk.aiff'`;
       }
     },
   };
 };
+
+async function isChildSession(
+  client: PluginInput["client"],
+  sessionId: string,
+) {
+  const { data: session } = await client.session.get({
+    path: { id: sessionId },
+  });
+  return session?.parentID !== null;
+}
